938b2737145abf7e082e3d24d6f5b388288a60c1
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-05-13 17:10:31 -0400

    Version 1.0 build 1 uploaded

--- Quick Tip/Quick Tip/ContentView.swift
+++ Quick Tip/Quick Tip/ContentView.swift
@@ -12,15 +12,6 @@ import Combine
 struct ContentView: View {
     
     let deviceHeight: CGFloat = UIScreen.main.bounds.height
-
-    var iPhone6To8AndplusSeries : Bool {
-        if deviceHeight >= 736 {
-            return true
-        } else {
-            return false
-        }
-    }
-    
     var iPhoneSE : Bool {
         if deviceHeight <= 568 {
             return true
@@ -36,13 +27,12 @@ struct ContentView: View {
         return true
     }
 
-
-//    @State private var value = 0.0
+    @State private var value = 0.0
     @State private var refresh = false
     @State private var isShowCloseButton = false
+    @State var isNavigationBarHidden: Bool = true
     @ObservedObject var tipViewModel = TipViewModel()
     @Environment(\.colorScheme) var colorScheme
-    @State var isNavigationBarHidden: Bool = true
 
     init() {
         if colorScheme == .dark {
@@ -89,7 +79,7 @@ struct ContentView: View {
                             self.isShowCloseButton.toggle()
                         })
                         .padding(20)
-                            .padding(.top, iPhoneSE ? 3 : 3)
+                        .padding(.top, iPhoneSE ? 3 : 3)
                         .padding(.leading, 26)
                         .background(colorScheme == .dark ? Color.darkEnd : Color.offWhite)
                         .frame(height: iPhoneSE ? 56 : hasSafeArea ? 66 : 62)
@@ -102,8 +92,9 @@ struct ContentView: View {
                                 .shadow(color: colorScheme == .dark ? Color.darkStart : Color.white, radius: 2, x: -4, y: -4)
                                 .clipShape(RoundedRectangle(cornerRadius: 15))
                         )
-                        .modifier(TextFieldModifier())
-                        
+                        .cornerRadius(15)
+                        .keyboardType(.decimalPad)
+
                         HStack {
                             LinearGradient(gradient: Gradient(colors: [.darkBlueColor, .lightBlueColor]), startPoint: .top, endPoint: .bottom)
                                 .mask(Text("$").font(.system(size: iPhoneSE ? 22 : 26, weight: .semibold, design: .rounded))
@@ -244,7 +235,8 @@ struct ContentView: View {
                                     .frame(width: iPhoneSE ? 36 : 50, height: 30, alignment: .center)
                                     .multilineTextAlignment(.center)
                                     .keyboardType(.decimalPad)
-                            
+                                    .disabled(true)
+                                
                                 Button(action: {
                                     print("plus tapped")
                                     self.tipViewModel.increasePerson()
@@ -431,70 +423,3 @@ extension Color {
 //        return formatter.string(from: number)!
 //    }
 //}
-
-public extension UIDevice {
-
-    /// pares the deveice name as the standard name
-    var modelName: String {
-
-        #if targetEnvironment(simulator)
-            let identifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"]!
-        #else
-            var systemInfo = utsname()
-            uname(&systemInfo)
-            let machineMirror = Mirror(reflecting: systemInfo.machine)
-            let identifier = machineMirror.children.reduce("") { identifier, element in
-                guard let value = element.value as? Int8 , value != 0 else { return identifier }
-                return identifier + String(UnicodeScalar(UInt8(value)))
-            }
-        #endif
-
-        switch identifier {
-        case "iPod5,1":                                 return "iPod Touch 5"
-        case "iPod7,1":                                 return "iPod Touch 6"
-        case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
-        case "iPhone4,1":                               return "iPhone 4s"
-        case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
-        case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
-        case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
-        case "iPhone7,2":                               return "iPhone 6"
-        case "iPhone7,1":                               return "iPhone 6 Plus"
-        case "iPhone8,1":                               return "iPhone 6s"
-        case "iPhone8,2":                               return "iPhone 6s Plus"
-        case "iPhone9,1", "iPhone9,3":                  return "iPhone 7"
-        case "iPhone9,2", "iPhone9,4":                  return "iPhone 7 Plus"
-        case "iPhone8,4":                               return "iPhone SE"
-        case "iPhone10,1", "iPhone10,4":                return "iPhone 8"
-        case "iPhone10,2", "iPhone10,5":                return "iPhone 8 Plus"
-        case "iPhone10,3", "iPhone10,6":                return "iPhone X"
-        case "iPhone11,2":                              return "iPhone XS"
-        case "iPhone11,4", "iPhone11,6":                return "iPhone XS Max"
-        case "iPhone11,8":                              return "iPhone XR"
-        case "iPhone12,1":                              return "iPhone 11"
-        case "iPhone12,3":                              return "iPhone 11 Pro"
-        case "iPhone12,5":                              return "iPhone 11 Pro Max"
-        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
-        case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
-        case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
-        case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
-        case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
-        case "iPad6,11", "iPad6,12":                    return "iPad 5"
-        case "iPad7,5", "iPad7,6":                      return "iPad 6"
-        case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
-        case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
-        case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
-        case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
-        case "iPad6,3", "iPad6,4":                      return "iPad Pro 9.7 Inch"
-        case "iPad6,7", "iPad6,8":                      return "iPad Pro 12.9 Inch"
-        case "iPad7,1", "iPad7,2":                      return "iPad Pro (12.9-inch) (2nd generation)"
-        case "iPad7,3", "iPad7,4":                      return "iPad Pro (10.5-inch)"
-        case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":return "iPad Pro (11-inch)"
-        case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":return "iPad Pro (12.9-inch) (3rd generation)"
-        case "AppleTV5,3":                              return "Apple TV"
-        case "AppleTV6,2":                              return "Apple TV 4K"
-        case "AudioAccessory1,1":                       return "HomePod"
-        default:                                        return identifier
-        }
-    }
-
-}
