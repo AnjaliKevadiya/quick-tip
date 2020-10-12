7738a89dee0855b0b1c8645b6a2bb12d0d0ba46b
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-10-12 10:00:01 -0400

    testing

--- Quick Tip/Quick Tip/View/SettingsView.swift
+++ Quick Tip/Quick Tip/View/SettingsView.swift
@@ -19,6 +19,7 @@ struct SettingsView: View {
     var body: some View {
         
         NavigationView {
+            
             ZStack() {
                 
                 if colorScheme == .dark {
